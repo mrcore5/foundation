5585ce826d19a6ebd03e796292649653c7a424d8
Matthew Reschke <mail@mreschke.com>
2015-04-06 17:09:58 -0500

    dd helpers

--- Support/Helpers.php
+++ Support/Helpers.php
@@ -18,3 +18,51 @@ if ( ! function_exists('asset'))
 		return app('url')->asset($path, $secure);
 	}
 }
+
+if ( ! function_exists('dump'))
+{
+	function dump($data)
+	{
+		// Symfonys var-dummper
+		foreach (func_get_args() as $var) {
+			\Symfony\Component\VarDumper\VarDumper::dump($var);
+		}
+	}
+}
+
+if ( ! function_exists('dd'))
+{
+	function dd($data)
+	{
+		// Laravels (from Illuminate\Support\helpers.php)
+		array_map(function($x) { (new \Illuminate\Support\Debug\Dumper)->dump($x); }, func_get_args());
+		die;
+	}
+}
+
+if ( ! function_exists('kdump'))
+{
+	function kdump($data)
+	{
+		// Kint
+		Kint::dump($data);
+	}
+}
+
+if ( ! function_exists('kdd'))
+{
+	function kdd($data)
+	{
+		kdump($data);
+		die;
+	}
+}
+
+if ( ! function_exists('vdd'))
+{
+	function vdd($data)
+	{
+		var_dump($data);
+		die;
+	}
+}
